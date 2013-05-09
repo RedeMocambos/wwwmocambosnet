e31e1e2303ceb90883157588d409f5771f82a4bd
Baobaxia <baobaxia@acotirene.sarava.org>
2013-05-09 20:06:43 +0000

    Mudanças no bootstrap.min.css etc

--- wwwmocambosnet/views.py
+++ wwwmocambosnet/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from wwwmocambosnet.forms import ContactForm
+from django.views.generic.edit import FormView
 
 def display_meta(request):
     values = request.META.items()
@@ -7,3 +9,15 @@ def display_meta(request):
     for k, v in values:
         html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
     return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+
+class ContactView(FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super(ContactView, self).form_valid(form)
